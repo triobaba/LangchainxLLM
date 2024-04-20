2f166522e9d19df20062310e7f139f9f433a823e
Triumphant Akinola Babalola <97790701+triobaba@users.noreply.github.com>
2024-04-20 03:16:06 +0100

    Update prompts.py

--- app/prompts.py
+++ app/prompts.py
@@ -2,7 +2,7 @@
 from langchain.prompts import PromptTemplate
 
 template = """Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES"). 
-If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+If you don't know the answer,say that you don't know. Don't make things up.
 ALWAYS return a "SOURCES" field in your answer, with the format "SOURCES: <source1>, <source2>, <source3>, ...".
 
 QUESTION: {question}
